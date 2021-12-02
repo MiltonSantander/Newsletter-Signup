3b8857e01e2ec6ab3f09c73eef77b708622a8674
MiltonGarcia <milgarsan@gmail.com>
2021-12-01 22:53:12 -0300

    Commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+    const url = "https://us20.api.mailchimp.com/3.0/lists/42629f4a9c";
+    const options = {
+        method: "POST",
+        auth: "milton:88cda1de595c5b6a440051423a950599-us20"
+    };
+    const apiRequest = https.request(url, options, function (response) {
+        //statusMessage
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function (data) {
+            // console.log(JSON.parse(data));
+        });
+    });
+    // apiRequest.write(jsonData);
+    apiRequest.end();
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
+
+//API key
+//88cda1de595c5b6a440051423a950599-us20
+//Audience id
+//42629f4a9c
+//End Point
+//https://us20.api.mailchimp.com/3.0/
